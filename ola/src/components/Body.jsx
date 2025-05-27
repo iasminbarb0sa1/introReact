737ca01d53ea98ba02218d867718f0994761f487
iasminbarb0sa1 <iasminsouto61@gmail.com>
2025-05-26 22:20:30 -0300

    feat: introdução no React

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Body.css'
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dinos são legais</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste eum sequi nihil veniam accusantium omnis vel necessitatibus quod, repudiandae fugiat, aliquid veritatis earum maiores, aspernatur commodi tempore sint temporibus illum.</p>
+        </div>
+  )
+}
+
+export default Body
---
+++ ola/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className='Header'>
+            <h1>Dinossauros 🦖 </h1>
+        </div>
+    )
+}
+
+export default Header
